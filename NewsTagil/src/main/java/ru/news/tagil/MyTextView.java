56e17005e432e9933346153bbeff7da35fc95ffd
turbo-lover <xaxa1992@gmail.com>
2013-07-12 18:11:59 +0300

    добавил свой класс с заменой шрифтов

---
+++ NewsTagil/src/main/java/ru/news/tagil/MyTextView.java
@@ -0,0 +1,35 @@
+package ru.news.tagil;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by turbo_lover on 12.07.13.
+ */
+public class MyTextView extends TextView {
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "font.ttf");
+            setTypeface(tf);
+        }
+    }
+    }
+}

4f6bfccffb25b258aa771499fbe6433f52fb8c2a
turbo-lover <xaxa1992@gmail.com>
2013-07-15 16:37:03 +0300

    логин почти готов, за исключением todo  отметок

--- NewsTagil/src/main/java/ru/news/tagil/LoginActivity.java
+++ NewsTagil/src/main/java/ru/news/tagil/LoginActivity.java
@@ -1,16 +1,130 @@
 package ru.news.tagil;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by turbo_lover on 12.07.13.
  */
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
+
+    EditText login,password;
+    Button sign_in,to_registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        Initialize_Component();
+        SetEventListeners();
+
+    }
+
+    private void SetEventListeners() {
+        sign_in.setOnClickListener(this);
+        to_registration.setOnClickListener(this);
+    }
+
+    private void Initialize_Component() {
+        //TODO когда будет готова верстка, заменить айдишники
+        sign_in = (Button) findViewById(R.id.button2);
+        to_registration = (Button) findViewById(R.id.button1);
+        login = (EditText) findViewById(R.id.editText2);
+        password = (EditText) findViewById(R.id.editText1);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            //TODO когда будет готова верстка, заменить айдишники
+            case R.id.button1://registration
+                Registration();
+                break;
+            case R.id.button2://sign in
+                SignIn();
+                break;
+        }
+
+    }
+
+    private void SignIn() {
+       if( isValidateFieldsOk()) {
+           My_AsyncTask_Worker worker = new My_AsyncTask_Worker();
+           JSONObject sends_data = new JSONObject();
+
+           try {
+               sends_data.put("login", login.getText());
+               sends_data.put("pass", password.getText());
+
+               worker.execute(sends_data,getResources().getString(R.string.ServerAddress)+getResources().getString(R.string.loginUrl));
+               ParsingResponse(worker.get());
+           }
+           catch (JSONException e) {
+               e.printStackTrace();
+               Log.d("SIGN_IN_METHOD_JSONException",e.getMessage() + "____________" + e.toString());
+           }
+           catch (InterruptedException e) {
+               e.printStackTrace();
+               Log.d("SIGN_IN_METHOD_Interrupted",e.getMessage() + "____________" + e.toString());
+           }
+           catch (ExecutionException e) {
+               e.printStackTrace();
+               Log.d("SIGN_IN_METHOD_Execution",e.getMessage() + "____________" + e.toString());
+           }
+       } else {
+           Toast.makeText(this, R.string.login_fields_empty,Toast.LENGTH_SHORT).show();
+       }
+    }
+
+    private void ParsingResponse(JSONObject response) {
+        try {
+            String status = response.getString("status");
+
+            if(status.equals("ok")){
+                toNextActivity();
+            }
+            if(status.equals("error")){
+                Toast.makeText(this,R.string.login_error,Toast.LENGTH_SHORT).show();
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+            Log.d("PARSING_RESPONsE_JSON",e.getMessage());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Log.d("PARSING_RESPONsE_EXEP",e.getMessage());
+        }
+
+    }
+
+    private void toNextActivity() {
+       //TODO захуячить переход на активити на которую мы попадаем после входа
+    }
+
+    private boolean isValidateFieldsOk() {
+        if(login.getText().length()==0 || password.getText().length()==0) {
+            return false;
+        }
+        return true;
+    }
+
+    private void Registration() {
+        Intent intent = new Intent();
+
+
     }
 }

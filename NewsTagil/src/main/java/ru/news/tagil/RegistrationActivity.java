b432be39d4b382f9f7a8633d564e05dc59c8fe79
Tamlin <spyro@te.ne.ua>
2013-07-16 11:58:48 +0300

    Добавил переходы в логин активити, и добавил пустую Registration activity

---
+++ NewsTagil/src/main/java/ru/news/tagil/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package ru.news.tagil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alexander on 16.07.13.
+ */
+public class RegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+}

1c6bfd54d9f4e3c7f20f4ce885424c3093f13eee
Serj-Pogosov <pogosov.serj@gmail.com>
2013-07-16 17:54:51 +0300

    Нужны апк для Кирила.

--- NewsTagil/src/main/java/ru/news/tagil/MainActivity.java
+++ NewsTagil/src/main/java/ru/news/tagil/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contact_goals);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

14e8f8b544bb6810bf70ab61539fd6f8ee90a27c
Tamlin <spyro@te.ne.ua>
2013-07-17 16:10:57 +0300

    Переход с main на login

--- NewsTagil/src/main/java/ru/news/tagil/MainActivity.java
+++ NewsTagil/src/main/java/ru/news/tagil/MainActivity.java
@@ -1,22 +1,26 @@
 package ru.news.tagil;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.hello, menu);
-        return true;
+        View rl = findViewById(R.id.main_layout);
+        rl.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
 }

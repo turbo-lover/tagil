f0cafcf4851733aa6ca3ee0686b73ec9582b98cd
turbo-lover <xaxa1992@gmail.com>
2013-07-19 18:38:37 +0300

    залил картинки во все папки!

---
+++ NewsTagil/src/main/java/ru/news/tagil/composite/compositeMessageItem.java
@@ -0,0 +1,13 @@
+package ru.news.tagil.composite;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by turbo_lover on 19.07.13.
+ */
+public class compositeMessageItem extends RelativeLayout {
+    public compositeMessageItem(Context context) {
+        super(context);
+    }
+}

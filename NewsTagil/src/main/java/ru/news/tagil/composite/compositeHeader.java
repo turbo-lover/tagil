192b08c4731182473bafeb154802ea25a863c4f9
turbo-lover <xaxa1992@gmail.com>
2013-07-23 11:37:35 +0300

    добавил переходы с compositeHeader

--- NewsTagil/src/main/java/ru/news/tagil/composite/compositeHeader.java
+++ NewsTagil/src/main/java/ru/news/tagil/composite/compositeHeader.java
@@ -3,30 +3,30 @@ package ru.news.tagil.composite;/**
  */
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import ru.news.tagil.R;
+import ru.news.tagil.activity.activityAds;
+import ru.news.tagil.activity.activityNewsPreview;
+import ru.news.tagil.utility.onUpdateClickListener;
 
 public class compositeHeader extends RelativeLayout implements View.OnClickListener {
+    private final String TAG = "compositeHeader";
 
-    Button main;
-    Button contact;
-    Button ads;
-    Button update;
+    private Button main, contact, ads, update;
+    private TextView tomorrow, now;
+    private onUpdateClickListener listener = null;
 
-    public compositeHeader(Context context) {
+    public compositeHeader(Context context,String weather_now,String weather_tomorrow) {
         super(context);
         Initialize_Component();
         SetEventListeners();
-    }
-
-    private void SetEventListeners() {
-        main.setOnClickListener(this);
-        contact.setOnClickListener(this);
-        ads.setOnClickListener(this);
-        update.setOnClickListener(this);
+        UpdateWeather(weather_now,weather_tomorrow);
     }
 
     private void Initialize_Component() {
@@ -37,6 +37,19 @@ public class compositeHeader extends RelativeLayout implements View.OnClickListe
         contact = (Button) findViewById(R.id.composite_header_contact );
         ads = (Button) findViewById(R.id.composite_header_ads );
         update = (Button) findViewById(R.id.composite_header_update );
+        tomorrow = (TextView) findViewById(R.id.composite_header_tomorrow_content);
+        now = (TextView) findViewById(R.id.composite_header_now_content);
+    }
+
+    private void SetEventListeners() {
+        main.setOnClickListener(this);
+        contact.setOnClickListener(this);
+        ads.setOnClickListener(this);
+        update.setOnClickListener(this);
+    }
+
+    public void SetUpdateListener(onUpdateClickListener listener) {
+        this.listener = listener;
     }
 
     @Override
@@ -44,14 +57,43 @@ public class compositeHeader extends RelativeLayout implements View.OnClickListe
         switch (view.getId())
         {
             case R.id.composite_header_main :
+               try {
+                   Intent intent = new Intent(getContext(), activityNewsPreview.class);
+                   getContext().startActivity(intent);
+               } catch (Exception e) {
+                   Log.e(TAG, "Exception");
+                   e.printStackTrace();
+               }
             break;
             case R.id.composite_header_contact:
+                try {
+                    Intent intent = new Intent(getContext(), activityContact.class);
+                    getContext().startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception");
+                    e.printStackTrace();
+                }
             break;
             case R.id.composite_header_ads :
+                try {
+                    Intent intent = new Intent(getContext(), activityAds.class);
+                    getContext().startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception");
+                    e.printStackTrace();
+                }
             break;
             case R.id.composite_header_update :
+                if(listener!= null) listener.UpdateButtonClicks();
             break;
 
         }
+    }
+
+    public void UpdateWeather(String now, String tomorrow)
+    {
+        // alt + 0176 -> значек градуса
+        this.now.setText(now+" C°");
+        this.tomorrow.setText(tomorrow + " C°");
     }
 }
---
+++ NewsTagil/src/main/java/ru/news/tagil/utility/onUpdateClickListener.java
@@ -0,0 +1,8 @@
+package ru.news.tagil.utility;
+
+/**
+ * Created by turbo_lover on 23.07.13.
+ */
+public interface onUpdateClickListener {
+    void UpdateButtonClicks();
+}

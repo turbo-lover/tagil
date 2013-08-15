410ccb35aa37192cd1fdc06fb5e364de6a2d6251
turbo-lover <xaxa1992@gmail.com>
2013-08-15 17:07:10 +0300

    переверстанно

--- NewsTagil/src/main/java/ru/news/tagil/activity/activityNewsPreview.java
+++ NewsTagil/src/main/java/ru/news/tagil/activity/activityNewsPreview.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
-
 import org.json.JSONObject;
 import ru.news.tagil.R;
 import ru.news.tagil.composite.compositeFirstButton;

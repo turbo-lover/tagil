8c78d3ccbc3526c06d43493c85103150be94ff06
Tamlin <spyro@te.ne.ua>
2013-08-23 15:39:49 +0300

    main

--- NewsTagil/src/main/java/ru/news/tagil/activity/activityMain.java
+++ NewsTagil/src/main/java/ru/news/tagil/activity/activityMain.java
@@ -3,22 +3,33 @@ package ru.news.tagil.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
+import android.os.CountDownTimer;
 import android.widget.RelativeLayout;
 import ru.news.tagil.R;
 
-public class activityMain extends Activity implements View.OnClickListener {
+public class activityMain extends Activity  {
     private Intent i;
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.main);
         RelativeLayout rl = (RelativeLayout) findViewById(R.id.main_holder);
-        rl.setOnClickListener(this);
         i  = new Intent(this,activityNewsPreview.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
+
     @Override
-    public void onClick(View view) {
-        startActivity(i);
+    protected void onStart() {
+        super.onStart();
+        new CountDownTimer(1000*30, 1000*3) {
+            @Override
+            public void onTick(long l) {
+                startActivity(i);
+                cancel();
+            }
+            @Override
+            public void onFinish() {}
+        }.start();
     }
+
 }
